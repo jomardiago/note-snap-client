1d49cf61307a9cb1d795151a5f88df56baf86917
Jose <Jose.Diago@kyndryl.com>
2023-12-23 16:51:28 +0800

    Initial design for App Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 import { useGetNotes } from "./apis/notes-api";
 import MainLayout from "./components/layouts/main-layout";
+import { Button } from "./components/ui/button";
 import useSessionStore from "./stores/session-store";
 
 export default function App() {
   const { session } = useSessionStore();
   const notes = useGetNotes(session?.id);
-
-  if (notes.isLoading) {
-    return <span>Loading...</span>;
-  }
+  const [showNoteForm, setShowNoteForm] = useState(false);
 
   return (
     <MainLayout>
-      <div className="grid grid-cols-4 gap-8">
+      {notes.isLoading && <span>Loading...</span>}
+
+      {notes.data?.length === 0 && (
+        <p className="text-lg text-gray-700 mb-4">Start adding a new note...</p>
+      )}
+
+      <Button onClick={() => setShowNoteForm(true)}>New Note</Button>
+
+      <div className="grid grid-cols-4 gap-8 mt-4">
+        {showNoteForm && (
+          <div className="border shadow-md p-4 min-h-[200px]">
+            <span>Note Form</span>
+          </div>
+        )}
+
         {notes.data &&
           notes.data.map((note) => (
             <div className="border shadow-md p-4 min-h-[200px]">
