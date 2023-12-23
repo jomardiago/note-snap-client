98c0a413babb37f8d572325db066c1842ee2962e
Jose <Jose.Diago@kyndryl.com>
2023-12-23 18:42:05 +0800

    Initial design for update and delete buttons.

--- src/components/notes/notes-list.tsx
+++ src/components/notes/notes-list.tsx
@@ -1,4 +1,5 @@
 import { Note } from "@/apis/notes-api";
+import { Edit, X } from "lucide-react";
 
 type Props = {
   notes: Array<Note>;
@@ -8,8 +9,16 @@ export const NotesList = ({ notes }: Props) => {
   return (
     <>
       {notes.map((note) => (
-        <div className="border shadow-md p-4 min-h-[200px]">
-          <h2 className="text-lg font-semibold">{note.title}</h2>
+        <div className="border shadow-md p-4 min-h-[200px] relative">
+          <button className="absolute top-0 right-0 p-2">
+            <X className="w-4 h-4 text-red-700" />
+          </button>
+          <div className="flex items-center gap-2">
+            <h2 className="text-lg font-semibold">{note.title}</h2>
+            <button>
+              <Edit className="w-4 h-4 text-yellow-500" />
+            </button>
+          </div>
           <p className="text-gray-700 mt-4">{note.description}</p>
         </div>
       ))}
